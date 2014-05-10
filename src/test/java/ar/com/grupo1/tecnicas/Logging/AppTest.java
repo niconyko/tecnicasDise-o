a83f86aa6c1c65529663c0bb44bb589c381b2775
jorgesmu <jorgsmu@gmail.com>
2014-05-10 12:36:54 -0300

    Estructura inicial

---
+++ src/test/java/ar/com/grupo1/tecnicas/Logging/AppTest.java
@@ -0,0 +1,14 @@
+package ar.com.grupo1.tecnicas.Logging;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
